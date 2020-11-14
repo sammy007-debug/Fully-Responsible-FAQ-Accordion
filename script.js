4083661d6ec36919de12db36a21f9c8da615cfed
samuel <samuelolayemi221@yahoo.com>
2020-11-14 01:07:48 +0100

    desktop version with js

---
+++ script.js
@@ -0,0 +1,14 @@
+this.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+    else{
+    questions.forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active")
+        }
+
+    }))
+}) 
